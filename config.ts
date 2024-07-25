57a3041943fd652255cb59b91f1a0233dec5a680
GitHub Action <action@github.com>
2024-07-25 07:45:30 +0000

    Automated commit message

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/pfr-app-content.git",
-  content_tag_latest: "1.1.10",
+  content_tag_latest: "1.1.11",
 };
 
 config.error_logging = {
