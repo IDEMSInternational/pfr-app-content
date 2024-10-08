fef05a39c8ec6f5fad72d93d5c1dc795ca1944ce
EsmeeIDEMS <esmee@idems.international>
2024-10-08 17:00:49 +0100

    content: 1.1.13

--- config.ts
+++ config.ts
@@ -13,7 +13,7 @@ config.google_drive = {
 
 config.git = {
   content_repo: "https://github.com/IDEMSInternational/pfr-app-content.git",
-  content_tag_latest: "1.1.12",
+  content_tag_latest: "1.1.13",
 };
 
 config.error_logging = {
